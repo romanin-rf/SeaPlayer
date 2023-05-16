3fadc2e6b9189922ab45b578601e664374a4f2ff
Romanin <60302782+romanin-rf@users.noreply.github.com>
2023-05-17 01:13:40 +0200

    Update test_functions.py

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,6 +1,10 @@
 import os
 import pytest
-from seaplayer.functions import is_midi_file
+try:
+    from seaplayer.functions import is_midi_file
+    IMPORTED = True
+except:
+    IMPORTED = False
 
 # ! Vars
 LOCAL_DIRPATH = os.path.dirname(__file__)
@@ -10,6 +14,7 @@ def get_clibration_filepath(filename: str) -> str:
     return os.path.join(LOCAL_DIRPATH, "calibration_data", filename)
 
 # ! Tests
-def test_is_midi_file():
-    assert is_midi_file(get_clibration_filepath("test.mid"))
+if IMPORTED:
+    def test_is_midi_file():
+        assert is_midi_file(get_clibration_filepath("test.mid"))
 
