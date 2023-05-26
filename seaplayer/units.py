9df633548dc230885e487d188c232ecb1420526b
Romanin <60302782+romanin-rf@users.noreply.github.com>
2023-05-27 00:20:57 +0200

    Version update

--- seaplayer/units.py
+++ seaplayer/units.py
@@ -6,7 +6,7 @@ from PIL.Image import Resampling
 
 # ! Metadata
 __title__ = "SeaPlayer"
-__version__ = "0.5.3.dev1"
+__version__ = "0.5.3.dev2"
 __author__ = "Romanin"
 __email__ = "semina054@gmail.com"
 __url__ = "https://github.com/romanin-rf/SeaPlayer"
