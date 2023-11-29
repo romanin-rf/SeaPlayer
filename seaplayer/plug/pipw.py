1edb04075109700adf1475b9d01be9a301224bae
Romanin <60302782+romanin-rf@users.noreply.github.com>
2023-11-29 23:33:52 +0200

    Update 0.7.5.post5
    - Fixed CLI operation
    - Fixed the `pip install` startup error

--- seaplayer/plug/pipw.py
+++ seaplayer/plug/pipw.py
@@ -7,12 +7,18 @@ class PIPManager:
     def __init__(self) -> None:
         self.python_path = sys.executable
     
-    def __call__(self, *args: str) -> Tuple[int, str]:
-        return subprocess.getstatusoutput(" ".join([self.python_path, "-m", *args]))
+    def __call__(self, *args: str) -> str:
+        return subprocess.check_output(
+            [self.python_path, "-m", "pip", *args],
+            stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        ).decode(errors="ignore")
     
-    def install(self, *args: str) -> bool:
-        code, output = self.__call__("install", *args)
-        return code == 0
+    def install(self, *args: str) -> str:
+        return self.__call__("install", *args)
+    
+    def install_requirements(self, filepath: str, upgrade: bool=False) -> str:
+        cmd = ["-U", "-r", filepath] if upgrade else ["-r", filepath]
+        return self.install(*cmd)
 
 # ! Initial
 pip = PIPManager()
