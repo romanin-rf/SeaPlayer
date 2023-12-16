c27149912367d009e2214fc4827d8c826f2bbc4c
Romanin <60302782+romanin-rf@users.noreply.github.com>
2023-12-16 13:53:42 +0200

    Update test_languages.py

--- tests/test_languages.py
+++ tests/test_languages.py
@@ -8,7 +8,7 @@ ll = LanguageLoader(LANGUAGES_DIRPATH, "en-eng")
 
 # ! Tests
 def test_language_count_loaded():
-    assert len(ll.alangs) >= len(os.listdir(LANGUAGES_DIRPATH))
+    assert len(ll.langs) >= len(os.listdir(LANGUAGES_DIRPATH))
 
 def test_language_get_not_exist():
     assert ll.get("") == "<LTNF>"
